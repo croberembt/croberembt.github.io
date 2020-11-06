7200626992d141b65d9e2d79396229a30be15108
Calli Oberembt <croberembt@gmail.com>
2020-11-06 15:11:40 -0600

    redeploying

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          My porfolio 
+          My portfolio 
         </p>
       </header>
     </div>
