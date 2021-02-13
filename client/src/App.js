a1d3ebd86da4fc89c94ff80382cd9083df1fccc6
Calli Oberembt <croberembt@gmail.com>
2021-02-13 15:57:45 -0600

    cleaning up app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'; 
-import Main from './components/MainComponent'; 
 import './App.css';
 
 
@@ -8,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
         <div className="App">
-            <Main />
+            My Portfolio
         </div>
     </BrowserRouter>
   );
