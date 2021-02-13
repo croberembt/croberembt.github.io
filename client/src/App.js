e377c9d946792fb7d0a449b31d130fc35d5b8eb9
Calli Oberembt <croberembt@gmail.com>
2021-02-13 16:08:31 -0600

    working on background image

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'; 
+import background from './images/background.jpg'; 
 import './App.css';
 
 
 function App() {
   return (
     <BrowserRouter>
-        <div className="App" style={{backgroundColor: 'purple'}}>
-            My Portfolio
+        <div className="App" style={{backgroundImage: `url(${background})`}}>
+            My Portfolio is cool
         </div>
     </BrowserRouter>
   );
