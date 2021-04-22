a3b3083ce514256b7fac6c4653481644682a4327
Calli Oberembt <croberembt@gmail.com>
2021-04-21 19:17:49 -0500

    played around with css flexbox

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -5,31 +5,29 @@ const Projects = () => {
 
   return (
     <div className='projects'>
-      <div className='container'>
 
-          <div className='project-card'>
-            <div><h2>Full Stack React Online Store</h2></div>
-            <div><h4>Full Stack React Online Store</h4></div>
-            <div>Full Stack React Online Store</div>
-            <a href="#">See It Live</a>
-          </div>
-     
-          <div className='project-card'>
-            <div><h2>Full Stack React Travel Log</h2></div>
-            <div><h4>Full Stack React Travel Log</h4></div>
-            <div>Full Stack React Travel Log</div>
-            <a href="#">See It Live</a>
-          </div>
-
-
-          <div className='project-card'>
-            <div><h2>Full Stack React To Do List</h2></div>
-            <div><h4>Full Stack React and Postgres To Do List</h4></div>
-            <div>Full Stack React To Do List</div>
-            <a href="#">See It Live</a>
-          </div>
-
+      <div className='projects-group'>
+        <div className='project-card'>
+          <h2>Full Stack React Online Store</h2>
+          <h4>Full Stack React Online Store</h4>
+          <div>Full Stack React Online Store</div>
+          <a href="#">See It Live</a>
         </div>
+
+        <div className='project-card'>
+          <h2>Full Stack React Travel Log</h2>
+          <h4>Full Stack React Travel Log</h4>
+          <div>Full Stack React Travel Log</div>
+          <a href="#">See It Live</a>
+        </div>
+      </div>
+
+      <div className='project-card'>
+        <h2>Full Stack React To Do List</h2>
+        <h4>Full Stack React and Postgres To Do List</h4>
+        <div>Full Stack React To Do List</div>
+        <a href="#">See It Live</a>
+      </div>
     </div>
   );
 }
