8c40fe684dc2951900f9106d2006c73f5ba5477f
Calli Oberembt <croberembt@gmail.com>
2021-04-22 20:06:54 -0500

    working on css flex

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -7,6 +7,7 @@ const Projects = () => {
     <div className='projects'>
 
       <div className='projects-group'>
+
         <div className='project-card'>
           <h2>Full Stack React Online Store</h2>
           <h4>Full Stack React Online Store</h4>
@@ -20,14 +21,16 @@ const Projects = () => {
           <div>Full Stack React Travel Log</div>
           <a href="#">See It Live</a>
         </div>
+
+        <div className='project-card'>
+          <h2>Full Stack React To Do List</h2>
+          <h4>Full Stack React and Postgres To Do List</h4>
+          <div>Full Stack React To Do List</div>
+          <a href="#">See It Live</a>
+        </div>
+        
       </div>
 
-      <div className='project-card'>
-        <h2>Full Stack React To Do List</h2>
-        <h4>Full Stack React and Postgres To Do List</h4>
-        <div>Full Stack React To Do List</div>
-        <a href="#">See It Live</a>
-      </div>
     </div>
   );
 }
