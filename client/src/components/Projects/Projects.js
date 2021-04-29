6a61a0d39e2b343241ae9095afa68538f366baff
Calli Oberembt <croberembt@gmail.com>
2021-04-28 23:01:30 -0500

    adjusted default px size to 10px, updated styles

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -9,21 +9,21 @@ const Projects = () => {
       <div className='projects-group'>
 
         <div className='project-card'>
-          <h2>Full Stack React Online Store</h2>
+          <h1>Full Stack React Online Store</h1>
           <h4>Full Stack React Online Store</h4>
           <div>Full Stack React Online Store</div>
           <a href="#">See It Live</a>
         </div>
 
         <div className='project-card'>
-          <h2>Full Stack React Travel Log</h2>
+          <h1>Full Stack React Travel Log</h1>
           <h4>Full Stack React Travel Log</h4>
           <div>Full Stack React Travel Log</div>
           <a href="#">See It Live</a>
         </div>
 
         <div className='project-card'>
-          <h2>Full Stack React To Do List</h2>
+          <h1>Full Stack React To Do List</h1>
           <h4>Full Stack React and Postgres To Do List</h4>
           <div>Full Stack React To Do List</div>
           <a href="#">See It Live</a>
