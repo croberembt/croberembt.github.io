af82e97ae3131ba168c8238f1b34c5c15cd13640
Calli Oberembt <croberembt@gmail.com>
2021-09-05 09:19:17 -0500

    updated to yarn package manager, added 2 live links for personal projects

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -11,11 +11,11 @@ const Projects = () => {
       <div className='projects-group'>
 
         <div className='project-card'>
-          <img src={ShoppingCartPhoto} />
+          <img src={ShoppingCartPhoto} alt='a screenshot of my shopping cart portfolio website' />
           <h1>Full Stack React Online Store</h1>
           <h4>Full Stack React Online Store</h4>
           <div>Full Stack React Online Store</div>
-          <a href="#">See It Live</a>
+          <a href='https://react-shopping-cart-test3.herokuapp.com/' target='_blank' rel='noreferrer'>See It Live</a>
         </div>
 
         <div className='project-card'>
@@ -23,7 +23,7 @@ const Projects = () => {
           <h1>Full Stack React Travel Log</h1>
           <h4>Full Stack React Travel Log</h4>
           <div>Full Stack React Travel Log</div>
-          <a href="#">See It Live</a>
+          <a href='#'>See It Live</a>
         </div>
         
       </div>
@@ -32,10 +32,10 @@ const Projects = () => {
 
         <div className='project-card'>
           <img src={TravelLogPhoto} />
-          <h1>Full Stack React Travel Log</h1>
+          <h1>Full Stack To Do List With Postgres</h1>
           <h4>Full Stack React Travel Log</h4>
           <div>Full Stack React Travel Log</div>
-          <a href="#">See It Live</a>
+          <a href='https://portfolio-postgres-to-do-list.herokuapp.com/' target='_blank' rel='noreferrer'>See It Live</a>
         </div>
 
         <div className='project-card'>
@@ -43,7 +43,7 @@ const Projects = () => {
           <h1>Full Stack React To Do List</h1>
           <h4>Full Stack React and Postgres To Do List</h4>
           <div>Full Stack React To Do List</div>
-          <a href="#">See It Live</a>
+          <a href='#'>See It Live</a>
         </div>
 
       </div>
